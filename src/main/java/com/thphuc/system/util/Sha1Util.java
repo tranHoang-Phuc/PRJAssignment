f5228ae57dc3262a6b4ba2aa49a10e64dd38f0dd
phuc11052003 <hoangphuc7bdtd@gmail.com>
2023-10-02 07:55:37 +0100

    update SessionUtil, Sha1Util

--- src/main/java/com/thphuc/system/util/Sha1Util.java
+++ src/main/java/com/thphuc/system/util/Sha1Util.java
@@ -4,10 +4,29 @@
  */
 package com.thphuc.system.util;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 /**
  *
  * @author tran Hoang Phuc
  */
 public class Sha1Util {
-    
+
+    public static String toSHA1(String password) {
+        String salt = "oasmstu423#nf49@nf;.c";
+        String result = null;
+        try {
+            String saltedPassword = password + salt;
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.update(saltedPassword.getBytes("UTF-8"));
+            byte[] hashBytes = md.digest();
+            result = Base64.getEncoder().encodeToString(hashBytes);
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
 }
